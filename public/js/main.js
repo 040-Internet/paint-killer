0dc68a4fa9781c3f2d778f146d66456a31a9ab18
connyz <connymartinsson@yahoo.se>
2014-10-28 11:46:07 +0100

    Added jquery and added a some working code to main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,71 @@
  */
 
 function Main(){
+    var self = this;
+    this.clickX = new Array();
+    this.clickY = new Array();
+    this.clickDrag = new Array();
+    this.paint = false;
     var c = document.getElementById("canvas");
     this.ctx = c.getContext("2d");
     this.ctx.fillStyle = "#FF0000";
     this.ctx.fillRect(0,0,150,75);
+    $('#canvas').mousedown(function(e){
+        self.mouseDownEvent(e);
+    });
+    $('#canvas').mouseup(function(e){
+        self.mouseUpEvent(e);
+    });
+    $('#canvas').mousemove(function(e){
+        self.mouseMoveEvent(e);
+    });
 }
 
+Main.prototype.mouseDownEvent = function(e){
+    var $canvas = $('#canvas').get(0);
+    var mouseX = e.pageX - $canvas.offsetLeft;
+    var mouseY = e.pageY - $canvas.offsetTop;
 
-Main.prototype.run = function(){
+    this.paint = true;
+    this.addClick(e.pageX - $canvas.offsetLeft, e.pageY - $canvas.offsetTop);
+    this.redraw();
+}
 
+Main.prototype.mouseUpEvent = function(e){
+    this.paint = false;
+}
+
+Main.prototype.mouseMoveEvent = function(e){
+    if(this.paint){
+        var $canvas = $('#canvas').get(0);
+        this.addClick(e.pageX - $canvas.offsetLeft, e.pageY - $canvas.offsetTop, true);
+        this.redraw();
+    }
+}
+
+Main.prototype.addClick = function(x, y, dragging){
+    this.clickX.push(x);
+    this.clickY.push(y);
+    this.clickDrag.push(dragging);
+}
+
+Main.prototype.redraw = function(){
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height); // Clears the canvas
+    this.ctx.strokeStyle = "#df4b26";
+    this.ctx.lineJoin = "round";
+    this.ctx.lineWidth = 5;
+
+    for(var i=0; i < this.clickX.length; i++) {
+        this.ctx.beginPath();
+        if(this.clickDrag[i] && i){
+            this.ctx.moveTo(this.clickX[i-1], this.clickY[i-1]);
+        }else{
+            this.ctx.moveTo(this.clickX[i]-1, this.clickY[i]);
+        }
+        this.ctx.lineTo(this.clickX[i], this.clickY[i]);
+        this.ctx.closePath();
+        this.ctx.stroke();
+    }
 }
 
 Main.prototype.connect = function(){
